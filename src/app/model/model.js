d5b085c0ae1791dbd0f5150273625f474943fc23
Tomáš <galetkatomas@gmail.com>
2020-02-06 15:16:23 +0100

    Add props types to model

---
+++ src/app/model/model.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Model = ({modelId}) => {
+    return <></>
+};
+
+Model.propsTypes = {
+    modelId: PropTypes.number.isRequired
+}
+
+export default Model;
